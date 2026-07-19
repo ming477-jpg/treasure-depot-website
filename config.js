37eac0a26a164731005fd659a52fc8ef7771745c
ming477-jpg <ming477@icloud.com>
2026-07-18 22:54:07 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.TD_CONFIG = Object.freeze({
+  supabaseUrl: 'https://vucveemjuebtznswcdkk.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1Y3ZlZW1qdWVidHpuc3djZGtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNDU5MzksImV4cCI6MjA5OTkyMTkzOX0.BpGHEyBkZiTbzKjn94BWloRb5tONnVDZRry3KKn1Y5A',
+  adminEmail: 'treasuredepotva@gmail.com'
+});
